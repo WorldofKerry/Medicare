b9d4d8c3c834a1b8439a4af23c6eb6b421c8dcab
Kerry_Wang <kerrywang369@gmail.com>
2020-03-12 20:41:09 -0700

    added calendarview under fan 2

--- app/src/main/java/com/example/recyclerviewintablayout/CalendarActivity.java
+++ app/src/main/java/com/example/recyclerviewintablayout/CalendarActivity.java
@@ -1,4 +1,48 @@
 package com.example.recyclerviewintablayout;
 
-public class CalendarActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalendarActivity extends AppCompatActivity {
+    private CalendarView calendar;
+    private Button backToMenu;
+    private TextView dateText;
+    public static String dateSelected;
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        calendar = findViewById(R.id.calendar);
+        dateText = findViewById(R.id.dateText);
+
+        dateSelected = "";
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                dateSelected = year + " / " + month + " / " + dayOfMonth;
+                dateText.setText(dateSelected);
+                Log.d("Date Selected", dateSelected);
+            }
+        });
+
+        backToMenu = findViewById(R.id.backToMenu);
+        backToMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CalendarActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
