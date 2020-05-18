29fd8ab7dc08a7efb76ae088ba00473c8def4702
James Zhong <j.zhong069@gmail.com>
2020-05-17 18:47:01 -0700

    Re-implemented calendar (event indicators and other cool things)

--- app/src/main/java/com/example/recyclerviewintablayout/CalendarActivity.java
+++ app/src/main/java/com/example/recyclerviewintablayout/CalendarActivity.java
@@ -1,98 +1,136 @@
 package com.example.recyclerviewintablayout;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
 import com.google.android.material.tabs.TabLayout;
+import com.google.gson.reflect.TypeToken;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class CalendarActivity extends AppCompatActivity {
-    private CalendarView calendar;
-    public static String dateSelected;
+	private CompactCalendarView calendar;
+	public static String dateSelected;
+	private List<Symptom> symptoms;
 
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-    private ViewPagerAdapter adapter;
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
+		final SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy", Locale.CANADA);
+		getSupportActionBar().setTitle(dateFormat.format(new Date()));
 
-        calendar = findViewById(R.id.calendar);
+		setContentView(R.layout.activity_calendar);
 
-        tabLayout = findViewById(R.id.calTabLayout);
-        viewPager = findViewById(R.id.calViewPager);
-        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+		// Calendar
 
-        adapter.AddFragment(new FragmentSymptom(), "Symptoms");
-        adapter.AddFragment(new FragmentBloodSugar(), "Blood Sugar");
-        adapter.AddFragment(new FragmentExercise(), "Exercise");
+		calendar = findViewById(R.id.calendar);
+		calendar.setUseThreeLetterAbbreviation(false);
+		calendar.shouldDrawIndicatorsBelowSelectedDays(true);
 
-        viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
+		setDateText(Calendar.getInstance().get(Calendar.YEAR),
+				Calendar.getInstance().get(Calendar.MONTH),
+				Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
 
-        dateSelected = "";
+		symptoms = (ArrayList<Symptom>) PrefSingleton.getInstance().LoadPreferenceList
+				("listSymptom",new TypeToken<ArrayList<Symptom>>() {}.getType());
 
-        FloatingActionButton floatingActionButton = findViewById(R.id.fab_action1);
-        floatingActionButton.setOnClickListener(new View.OnClickListener()      {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), SelectionActivity.class);
-                startActivity(intent);
-            }
-        });
+		setSymptomEvents();
 
-        FloatingActionButton floatingActionButton2 = findViewById(R.id.fab_action2);
-        floatingActionButton2.setOnClickListener(new View.OnClickListener()      {
-            @Override
-            public void onClick(View v) {
-                // Testing
-                Intent intent = new Intent(CalendarActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+		calendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+			@Override
+			public void onDayClick(Date dateClicked) {
 
-        FloatingActionButton floatingActionButton3 = findViewById(R.id.fab_action3);
-        floatingActionButton3.setOnClickListener(new View.OnClickListener()      {
-            @Override
-            public void onClick(View v) {
-                // Testing
-                Intent intent = new Intent(v.getContext(), MainReminderActivity.class);
-                startActivity(intent);
-            }
-        });
+			}
 
-        FloatingActionButton floatingActionButton4 = findViewById(R.id.fab_action4);
-        floatingActionButton4.setOnClickListener(new View.OnClickListener()      {
-            @Override
-            public void onClick(View v) {
-                // Testing
-                Intent intent = new Intent(v.getContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
+			@Override
+			public void onMonthScroll(Date firstDayOfNewMonth) {
+				getSupportActionBar().setTitle(dateFormat.format(firstDayOfNewMonth));
+			}
+		});
 
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                dateSelected = year + "-" + (month + 1) + "-" + dayOfMonth;
-                Log.d("Date Selected", dateSelected);
-            }
-        });
-    }
+		// View Pager
 
-    public void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
+		TabLayout tabLayout = findViewById(R.id.calTabLayout);
+		ViewPager viewPager = findViewById(R.id.calViewPager);
+		ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+		adapter.AddFragment(new FragmentSymptom(), "Symptoms");
+		adapter.AddFragment(new FragmentBloodSugar(), "Blood Sugar");
+		adapter.AddFragment(new FragmentExercise(), "Exercise");
+
+		viewPager.setAdapter(adapter);
+		tabLayout.setupWithViewPager(viewPager);
+
+		FloatingActionButton floatingActionButton = findViewById(R.id.fab_action1);
+		floatingActionButton.setOnClickListener(new View.OnClickListener()      {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), SelectionActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		FloatingActionButton floatingActionButton2 = findViewById(R.id.fab_action2);
+		floatingActionButton2.setOnClickListener(new View.OnClickListener()      {
+			@Override
+			public void onClick(View v) {
+				// Testing
+				Intent intent = new Intent(CalendarActivity.this, MainActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		FloatingActionButton floatingActionButton3 = findViewById(R.id.fab_action3);
+		floatingActionButton3.setOnClickListener(new View.OnClickListener()      {
+			@Override
+			public void onClick(View v) {
+				// Testing
+				Intent intent = new Intent(v.getContext(), MainReminderActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		FloatingActionButton floatingActionButton4 = findViewById(R.id.fab_action4);
+		floatingActionButton4.setOnClickListener(new View.OnClickListener()      {
+			@Override
+			public void onClick(View v) {
+				// Testing
+				Intent intent = new Intent(v.getContext(), MainActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+	private void setDateText(int year, int month, int dayOfMonth) {
+		dateSelected = year + "-" + (month + 1) + "-" + dayOfMonth;
+	}
+
+	private void setSymptomEvents() {
+		for(int i = 0; i < symptoms.size(); i++) {
+			final String[] date = symptoms.get(i).getTime().split("[-+:]");
+
+			Calendar c = Calendar.getInstance();
+			c.set(Integer.parseInt(date[0]), Integer.parseInt(date[1]) - 1, Integer.parseInt(date[2]), Integer.parseInt(date[3]), Integer.parseInt(date[4]));
+
+			Log.d("Epoch Time", "" + c.getTimeInMillis());
+			Event event = new Event(Color.RED, c.getTimeInMillis());
+
+			calendar.addEvent(event);
+		}
+	}
 }
